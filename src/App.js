2b56200f62b1a071461019857b4b82041d9d57b9
Kareem Amara <kareemamarah2356@gmail.com>
2022-03-13 14:28:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
-const API_KEY = `ae62cb03104cecee740cbc51b945bfb2`;
-const API_REQUEST = `https://api.themoviedb.org/3/movie/550?api_key=ae62cb03104cecee740cbc51b945bfb2`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=ae62cb03104cecee740cbc51b945bfb2&query=`;
-const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=ae62cb03104cecee740cbc51b945bfb2&language=en-US&page=1`;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
